6b1e54da759d2cafa04051cb7024e0bc3a6bf1d8
VNavrockij <vitalik.nav@gmail.com>
2023-10-10 14:53:22 +0300

    add: Alamofire

---
+++ drumncode-Weather-Test/Model/WeatherAPI.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  drumncode-Weather-Test
+//
+//  Created by Vitalii Navrotskyi on 10.10.2023.
+//
+
+import Foundation
+
+struct WeatherAPI {
+    var key: String {
+        get {
+            return "1f4cb4b07b1349fa90194950231010"
+        }
+    }
+}
